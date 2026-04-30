cbe6fd29227090f2ae75693a8bc65153c6b60b2c
michi <michelleoliviah16@gmail.com>
2026-04-30 11:42:47 +0700

    mongoconnect

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,20 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors()); //enable cross-origin requests
-app.use(express.json()); //read request body as JSON
+app.use(cors());
+app.use(express.json());
+
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected ✅"))
+  .catch(err => console.error(err));
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
