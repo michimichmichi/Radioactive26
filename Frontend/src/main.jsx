bef9b681257cd0701bc511d9e3b5f4fddab579f4
Michael Immanuel Herijanto <57061376+michaelimmanuel@users.noreply.github.com>
2026-06-29 22:26:44 +0700

    admin panel and few contoller

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import AdminPage from './Pages/Admin.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AdminPage />
   </StrictMode>,
 )
