9259c7c09416ab1c88d61191a97e4f9d3e91df4a
Ciel806 <ceryne688@gmail.com>
2026-06-29 23:23:31 +0700

    navbar

--- Frontend/src/Pages/Hero.jsx
+++ Frontend/src/Pages/Hero.jsx
@@ -4,7 +4,14 @@ import Navbar from '../Components/NavigationBar';
 
 function Hero() {
   return (
-    <div className="bg-[#AD0056] h-screen items-center justify-center">
+    <div className="bg-black h-screen items-center justify-center"
+          style={{
+            backgroundColor: "#050505",
+            backgroundImage:
+              "radial-gradient(circle, rgba(255,255,255,0.1) 1.2px, transparent 1px)",
+            backgroundSize: "5px 5px",
+          }}
+    >
 
       <Navbar />
       
@@ -12,12 +19,12 @@ function Hero() {
         <div className='absolute bottom-0 right-0 z-10'>
          <img
           src={mic}
-          className="h-500 w-500"
+          className="h-[500px] w-[500px] object-contain"
         />
         </div>
         <FuzzyText
-        fontSize='clamp(3rem, 15vw, 10rem)'
-          fontFamily="Bitcount Prop Double Ink"
+          fontSize='clamp(3rem, 15vw, 10rem)'
+          fontFamily="bitcount" /* <-- Keep this strictly lowercase 'bitcount' */
           baseIntensity={0.08}
           hoverIntensity={0.07}
           enableHover
