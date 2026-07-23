d08ba0be2b299da74029b5277b109c5b8284b846
michimichmichi <michelleoliviah16@gmail.com>
2026-07-23 17:19:51 +0700

    gallery

--- Frontend/src/Components/Gallery.jsx
+++ Frontend/src/Components/Gallery.jsx
@@ -10,12 +10,22 @@ import photo2 from '../assets/gallery/2.JPG';
 import photo3 from '../assets/gallery/3.JPG';
 import photo4 from '../assets/gallery/4.JPG';
 import photo5 from '../assets/gallery/5.JPG';
-import photo6 from '../assets/gallery/6.JPG';
 import photo7 from '../assets/gallery/7.JPG';
 import photo8 from '../assets/gallery/8.JPG';
 import photo9 from '../assets/gallery/9.JPG';
 import photo10 from '../assets/gallery/10.PNG';
 import photo11 from '../assets/gallery/11.PNG';
+import photo12 from '../assets/gallery/12.png';
+import photo13 from '../assets/gallery/13.png';
+import photo14 from '../assets/gallery/14.png';
+import photo15 from '../assets/gallery/15.png';
+import photo16 from '../assets/gallery/16.png';
+import photo17 from '../assets/gallery/17.png';
+import photo18 from '../assets/gallery/18.png';
+import photo19 from '../assets/gallery/19.png';
+import photo20 from '../assets/gallery/20.png';
+
+
 
 export default function Gallery() {
   const photos = [
@@ -24,12 +34,20 @@ export default function Gallery() {
   photo3,
   photo4,
   photo5,
-  photo6,
   photo7,
   photo8,
   photo9,
   photo10,
   photo11,
+  photo12,
+  photo13,
+  photo14,
+  photo15,
+  photo16,
+  photo17,
+  photo18,
+  photo19,
+  photo20
 ];
 
   return (
